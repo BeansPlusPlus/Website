c70ce0b3f7dfdc4bf9102a04797fd8a274ca7bad
Tominne <108726281+Tominne@users.noreply.github.com>
2023-03-02 12:42:47 +1300

    ITS WORKING OML

--- www/help.js
+++ www/help.js
@@ -5,5 +5,6 @@ sayhi();
 
 
 document.getElementById('travel').onclick = () => {
-    document.getElementById('take').innerHTML = 'Cool place, you should visit!';
+    document.getElementById('take').innerHTML = 'Cool place, you should visit!'
+    document.getElementById('travel').style.display = 'margin:auto' 'color:yellow';
 }
