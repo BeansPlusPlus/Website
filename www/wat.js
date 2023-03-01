2bdda2eb569514e33bb6815eaf24156dd11078f5
Tominne <108726281+Tominne@users.noreply.github.com>
2023-03-01 14:20:31 +1300

    Making weird button

---
+++ www/wat.js
@@ -0,0 +1,9 @@
+javascript
+function sayhi() {return "Hi";}
+sayhi();
+
+<button onclick="document.getElementById('myImage').src='pic_bulbon.gif'">Turn on the light</button>
+
+<img id="myImage" src="pic_bulboff.gif" style="width:100px">
+
+<button onclick="document.getElementById('myImage').src='pic_bulboff.gif'">Turn off the light</button>
