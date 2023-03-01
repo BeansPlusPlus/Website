354dfd6167710a5b20bb27b12ed8cd51bf7cdcae
Tominne <108726281+Tominne@users.noreply.github.com>
2023-03-02 12:05:50 +1300

    fix

--- www/help.js
+++ www/help.js
@@ -3,8 +3,8 @@ function sayhi() { return "Hi"; }
 sayhi();
 
 const Name = "Beans Beloved";
-var
-function;
-    document.getElementById;("travel").onclick = function () {
+
+
+document.getElementById;("travel").onclick = function () {
         document.getElementById("take").innerHTML = "Cool game, you should try it out!";
-        document.getElementById;("travel").style.display = } 
+        document.getElementById;("travel").style.display = "yellow";} 
