521c3f4febfc0de1515d0a5779c1b6d5accbff23
Tominne <108726281+Tominne@users.noreply.github.com>
2023-03-02 12:37:39 +1300

    help

--- www/help.js
+++ www/help.js
@@ -4,5 +4,6 @@ function sayhi() { return "Hi"; }
 sayhi();
 
 
-document.getElementById('travel').onclick = function () {
-              document.getElementById('take').innerHTML = 'Cool place, you should visit!';}
+document.getElementById('travel').onclick = () => {
+    document.getElementById('take').innerHTML = 'Cool place, you should visit!';
+}
