5e93f4cb5459b6254c27a0a64e7ae420a5abcdaf
Tominne <108726281+Tominne@users.noreply.github.com>
2023-03-02 12:34:50 +1300

    est

--- www/help.js
+++ www/help.js
@@ -4,9 +4,6 @@ function sayhi() { return "Hi"; }
 sayhi();
 <div>
 
-    document.getElementById;("travel").onclick = function () {
-        document.getElementById("take").innerHTML = "Cool place, you should visit!"}
+<button onclick="document.getElementById('travel').onclick = function () {
+              document.getElementById('take').innerHTML = 'Cool place, you should visit!' }
 
-    document.getElementById;("boop").onclick = function () {
-        document.getElementById("take").innerHTML = "This place is aight"}
-</div>
