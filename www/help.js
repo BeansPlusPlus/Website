277c64d53e7732f562757e8b946b78ea3dffc6c2
Tominne <108726281+Tominne@users.noreply.github.com>
2023-03-02 09:26:55 +1300

    fixed type error

--- www/help.js
+++ www/help.js
@@ -3,6 +3,8 @@ function sayhi() { return "Hi"; }
 sayhi();
 
 const Name = "Beans Beloved";
-var 
+var
 
-document,getElementById,("travel").onclick = function() {document.getElementById("travel").innerHTML = "Cool game, you should try it out!";
+    document, getElementById("travel").onclick = function () {
+        document.getElementById("travel").innerHTML = "Cool game, you should try it out!";
+    }
