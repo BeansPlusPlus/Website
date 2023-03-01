6777f204f4092ed823e3ff39a5b4ca6d6757498c
Tominne <108726281+Tominne@users.noreply.github.com>
2023-03-02 09:19:02 +1300

    javascript script add

---
+++ www/help.js
@@ -0,0 +1,14 @@
+javascript
+function sayhi() { return "Hi"; }
+sayhi();
+
+const Name = "Beans Beloved";
+var 
+
+<button onclick="document.getElementById('myImage').src='musiccorner.png' ">Turn on the light</button>
+
+<img id="myImage" src="EquestrianField.png" style="width:100px">
+
+<button onclick="document.getElementById('myImage').src='secretgarden.png' ">Turn off the light</button>
+
+document.getElementById("travel").onclick = function() {document.getElementById("travel").innerHTML = "Cool game, you should try it out!";
