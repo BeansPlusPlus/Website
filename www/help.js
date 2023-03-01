fc9b7d9ebe8fb1749db2e3aa980d8ebb83c2e74a
Tominne <108726281+Tominne@users.noreply.github.com>
2023-03-02 12:21:35 +1300

    idk why its not working

--- www/help.js
+++ www/help.js
@@ -1,14 +1,12 @@
 
-javascript
+
 function sayhi() { return "Hi"; }
 sayhi();
+<div>
 
-{const Name = "Beans Beloved"; }
+    document.getElementById;("travel").onclick = function () {
+        document.getElementById("take").innerHTML = "Cool place, you should visit!"}
 
-document.getElementById;("travel").onclick = function () {
-        document.getElementById("take").innerHTML = "Cool place, you should visit!";
-        document.getElementById;("travel").style.display = "color:yellow";} 
-
-document.getElementById;("boop").onclick = function () {
-            document.getElementById("take").innerHTML = "This place is aight";
-            document.getElementById;("travel").style.display = "color:yellow";} 
+    document.getElementById;("boop").onclick = function () {
+        document.getElementById("take").innerHTML = "This place is aight"}
+</div>
