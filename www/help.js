480f80a7db9084f620f4480f26a8e624e75542d0
Tominne <108726281+Tominne@users.noreply.github.com>
2023-03-02 09:33:25 +1300

    fixed?

--- www/help.js
+++ www/help.js
@@ -5,6 +5,7 @@ sayhi();
 const Name = "Beans Beloved";
 var
 
-    document, getElementById("travel").onclick = function () {
-        document.getElementById("travel").innerHTML = "Cool game, you should try it out!";
+    document.getElementById("travel").onclick = function () {
+        document.getElementById("take").innerHTML = "Cool game, you should try it out!";
+        document.getElementById 
     }
