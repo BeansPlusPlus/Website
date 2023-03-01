71aece80691757f61396fb667745875a8357e70a
Tominne <108726281+Tominne@users.noreply.github.com>
2023-03-02 09:22:52 +1300

    fixed type errors

--- www/help.js
+++ www/help.js
@@ -5,10 +5,4 @@ sayhi();
 const Name = "Beans Beloved";
 var 
 
-<button onclick="document.getElementById('myImage').src='musiccorner.png' ">Turn on the light</button>
-
-<img id="myImage" src="EquestrianField.png" style="width:100px">
-
-<button onclick="document.getElementById('myImage').src='secretgarden.png' ">Turn off the light</button>
-
-document.getElementById("travel").onclick = function() {document.getElementById("travel").innerHTML = "Cool game, you should try it out!";
+document,getElementById,("travel").onclick = function() {document.getElementById("travel").innerHTML = "Cool game, you should try it out!";
