7a6da7df88525de9512247e6436b294701992766
Tominne <108726281+Tominne@users.noreply.github.com>
2023-03-02 12:43:25 +1300

    fix

--- www/help.js
+++ www/help.js
@@ -6,5 +6,5 @@ sayhi();
 
 document.getElementById('travel').onclick = () => {
     document.getElementById('take').innerHTML = 'Cool place, you should visit!'
-    document.getElementById('travel').style.display = 'margin:auto' 'color:yellow';
+    document.getElementById('travel').style.display = 'margin:auto', 'color:yellow';
 }
